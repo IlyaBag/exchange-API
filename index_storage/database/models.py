b0874695ea9a71eab5216ee300d647c6a3b9716f
Ilya Bagrov <ilya.b.only@gmail.com>
2024-11-01 00:05:57 +0300

    Change the name and type of the timestamp field

--- index_storage/database/models.py
+++ index_storage/database/models.py
@@ -1,22 +1,20 @@
-from datetime import datetime
-
-from sqlalchemy import TIMESTAMP, String, text
+from sqlalchemy import String, text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True),
-        server_default=text("TIMEZONE('utc', now())"),
-    )
+    pass
 
 
 class IndexPriceModel(Base):
     __tablename__ = 'index_price'
 
+    id: Mapped[int] = mapped_column(primary_key=True)
     ticker: Mapped[str] = mapped_column(String(10))
     price: Mapped[float]
+    unix_timestamp: Mapped[float] = mapped_column(
+        server_default=text("extract(epoch from now())"),
+    )
 
     def __str__(self):
         return f'<{self.__class__.__name__}({self.id}), ticker:{self.ticker \
